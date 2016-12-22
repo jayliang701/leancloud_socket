253e2f31e601ecea291caa7c23fc32421c65e7ec
jayliang701 <156070304@qq.com>
2016-12-22 16:46:41 +0800

    update socket

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 /**
  * Created by Jay on 2016/12/22.
  */
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var liveNum = 0;
 
-app.get('/', function(req, res){
-    res.end(`current connection number: ${liveNum}`);
-});
-
+var BufferHelper = require('bufferhelper');
+var fs = require('fs');
+var io = require('socket.io')(3000);
 io.on('connection', function(socket){
     liveNum ++;
 
@@ -22,15 +17,37 @@ io.on('connection', function(socket){
         connectTime: Date.now()
     };
 
+    //自定义数据包
+    socket.sendAsBuffer = function(type, data) {
+        //var buffer = new BufferHelper();
+        data = data || {};
+        if (typeof data == "object") {
+            data = JSON.stringify(data);
+        }
+        /*
+        args.forEach(function(arg, i) {
+            if (typeof arg == "object") {
+                arg = JSON.stringify(arg);
+            }
+            arg = String(arg);
+            if (i < (args.length - 1)) {
+                arg = "&=";
+            }
+            buffer.concat(new Buffer(String(data)));
+        });
+        */
+        socket.emit(type, new Buffer(String(data)));
+    };
+
     console.log(`client *${socket.id}* connected from ${socket.info.ip}:${socket.info.port}`);
 
     /* 向客户端推送消息 */
     //例如当客户端连接后，马上发送一个欢迎的消息
-    socket.emit('welcome', { ip:socket.info.ip, port:socket.info.port });
+    socket.sendAsBuffer('welcome', { ip:socket.info.ip, port:socket.info.port });
 
     //例如定时发送消息给客户端
     socket.__timer = setInterval(function() {
-        socket.emit('time_update', { time:Date.now() });
+        socket.sendAsBuffer('time_update', { time: Date.now() });
     }, 3000);
 
     //////////////////////
@@ -52,6 +69,4 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
-});
+var liveNum = 0;
